6af11b2f53841f97efc05da44895aa5b0d3fdafc
ilona <isaevaangelina@gmail.com>
2021-03-13 01:33:55 +0200

    Initial commit

---
+++ tim.js
@@ -0,0 +1,5 @@
+function howManyTimes(annualPrice, individualPrice) {
+    let num = annualPrice / individualPrice
+    return Math.ceil(num)
+}
+//https://www.codewars.com/kata/57efcb78e77282f4790003d8
